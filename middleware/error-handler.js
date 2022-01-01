5225a0312663c1cad709af82638881c3c4be35f1
eriqneo <88620602+eriqneo@users.noreply.github.com>
2022-01-01 14:14:55 +0300

    Added security Packages

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,36 @@
-const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  //set custom error as default
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong Try Again later!!",
+  };
+
+  //Customise ValidationError ;
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  //set Duplicate Error
+  if (err.code || err.code === 11000) {
+    customError.msg = `Duplicate Value entered for ${Object.keys(
+      err.keyValue
+    )} field, Please Choose another Value`;
+    customError.statusCode = 400;
+  }
+
+  //set Cast Error
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id ${err.value}`;
+    customError.statusCode = 400;
+  }
+
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
